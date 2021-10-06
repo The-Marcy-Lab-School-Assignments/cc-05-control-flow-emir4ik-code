7309e89153dd097afedeb71fc11e4e6cac551591
emir4ik-code <emirahmetov@gmail.com>
2021-10-06 14:15:44 +0000

    problem 1 solved

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-//1
-function absoluteValue(){
-  
-}
+const absoluteValue = int => {
+    if(isNaN(int)) {
+        return 'null'
+        
+    }else if(int < 0) {
+        return (int  - (int * 2))
+    }else {
+        return int;
+    }
+} 
 
-
-
+//console.log(absoluteValue(35));
 
