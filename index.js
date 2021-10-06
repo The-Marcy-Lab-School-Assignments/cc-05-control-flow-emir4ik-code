9ac18f6c5c46f91df4590c8829e3c49bc7059956
emir4ik-code <emirahmetov@gmail.com>
2021-10-06 14:23:18 +0000

    problem 2 solved

--- index.js
+++ index.js
@@ -9,5 +9,18 @@ const absoluteValue = int => {
     }
 } 
 
-//console.log(absoluteValue(35));
+// console.log(absoluteValue(35));
+// console.log(absoluteValue(-35));
+// console.log(absoluteValue(3.5));
+// console.log(absoluteValue('avs'));
 
+
+const multiplesOfFourAndSix = () => {
+    let newArray = [];
+    for(let i = 1; i < 101; i++) {
+        if(i % 4 === 0 && i % 6 === 0) {
+            newArray.push(i);
+    }
+}return newArray;
+}
+//console.log(multiplesOfFourAndSix());
